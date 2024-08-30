8b59da4f13a15541ad16f4f238977bb1ef830961
StebenC <Stebenc09122002@gmail.com>
2024-08-30 20:25:20 +0700

    Animate for Login and Register Screen

--- ClientApp/app/src/main/java/com/example/clientapp/ui/LoginFragment.kt
+++ ClientApp/app/src/main/java/com/example/clientapp/ui/LoginFragment.kt
@@ -7,7 +7,9 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.example.clientapp.MainViewModel
+import com.example.clientapp.R
 import com.example.clientapp.databinding.FragmentLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,38 +20,100 @@ class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
     
+    private val screenWidth by lazy {
+        resources.displayMetrics.widthPixels.toFloat()
+    }
+    
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        setInitialPositions()
         return binding.root
     }
     
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
-        // Bind the service if not already bound
+        animateViewsIn()
         viewModel.bindService()
         
-        binding.loginButton.setOnClickListener {
-            val username = binding.usernameEditText.text.toString()
-            val password = binding.passwordEditText.text.toString()
-            if (username.isNotBlank() && password.isNotBlank()) {
-                val user = viewModel.login(username, password)
-                if (user != null) {
-                    Toast.makeText(context, "Login successful", Toast.LENGTH_SHORT).show()
-                    // Navigate to another fragment or perform an action
-                } else {
-                    Toast.makeText(context, "Login failed", Toast.LENGTH_SHORT).show()
-                }
-            } else {
-                Toast.makeText(
-                    context,
-                    "Please enter both username and password",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
+        binding.registerButton.setOnClickListener {
+            animateViewsOutAndNavigate()
         }
+        binding.loginButton.setOnClickListener {
+            handleLogin()
+        }
+    }
+    
+    private fun setInitialPositions() {
+        binding.registerButton.translationX = screenWidth
+        binding.loginTextView.translationX = -screenWidth
+        binding.enterYourEmailAndPasswordTextView.translationX = -screenWidth
+    }
+    
+    private fun animateViewsIn() {
+        binding.registerButton.animate()
+            .translationX(0f)
+            .setDuration(500)
+            .start()
+        
+        binding.loginTextView.animate()
+            .translationX(0f)
+            .setDuration(500)
+            .start()
+        
+        binding.enterYourEmailAndPasswordTextView.animate()
+            .translationX(0f)
+            .setDuration(500)
+            .start()
+    }
+    
+    private fun animateViewsOutAndNavigate() {
+        binding.enterYourEmailAndPasswordTextView.animate()
+            .translationX(-screenWidth)
+            .setDuration(500)
+            .start()
+        
+        binding.loginTextView.animate()
+            .translationX(-screenWidth)
+            .setDuration(500)
+            .start()
+        
+        binding.rememberMeLayout.animate()
+            .translationX(-screenWidth)
+            .setDuration(500)
+            .start()
+        
+        binding.registerButton.animate()
+            .translationX(screenWidth)
+            .setDuration(500)
+            .withEndAction {
+                findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+            }
+            .start()
+        
+    }
+    
+    private fun handleLogin() {
+        val username = binding.usernameEditText.text.toString()
+        val password = binding.passwordEditText.text.toString()
+        
+        if (username.isNotBlank() && password.isNotBlank()) {
+            val user = viewModel.login(username, password)
+            if (user != null) {
+                Toast.makeText(context, "Login successful", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(context, "Login failed", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Toast.makeText(context, "Please enter both username and password", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+    
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- ClientApp/app/src/main/java/com/example/clientapp/ui/RegisterFragment.kt
+++ ClientApp/app/src/main/java/com/example/clientapp/ui/RegisterFragment.kt
@@ -1,21 +1,83 @@
 package com.example.clientapp.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.clientapp.R
+import androidx.fragment.app.Fragment
+import com.example.clientapp.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
-
-
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
+    
+    private val screenWidth by lazy {
+        resources.displayMetrics.widthPixels.toFloat()
+    }
+    
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        setInitialPositions()
+        return binding.root
     }
-
+    
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        animateViewsIn()
+        binding.loginButton.setOnClickListener {
+            animateViewsOut()
+        }
+    }
+    
+    private fun setInitialPositions() {
+        binding.loginButton.translationX = -screenWidth
+        binding.registerTextView.translationX = screenWidth
+        binding.enterYourEmailAndPasswordTextView.translationX =
+            screenWidth
+    }
+    
+    private fun animateViewsIn() {
+        binding.loginButton.animate()
+            .translationX(0f)
+            .setDuration(500)
+            .start()
+        
+        binding.registerTextView.animate()
+            .translationX(0f)
+            .setDuration(500)
+            .start()                // Navigate back to the login fragment
+        
+        binding.enterYourEmailAndPasswordTextView.animate()
+            .translationX(0f)
+            .setDuration(500)
+            .start()
+    }
+    
+    private fun animateViewsOut() {
+        binding.enterYourEmailAndPasswordTextView.animate()
+            .translationX(screenWidth)
+            .setDuration(500)
+            .start()
+        
+        binding.registerTextView.animate()
+            .translationX(screenWidth)
+            .setDuration(500)
+            .start()
+        
+        binding.loginButton.animate()
+            .translationX(-screenWidth)
+            .setDuration(500)
+            .withEndAction {
+                requireActivity().onBackPressed()
+            }
+            .start()
+    }
+    
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
