076ddbb2fb69cbbf8b1c52c872e0b14a9012442e
StebenC <Stebenc09122002@gmail.com>
2024-09-06 10:25:19 +0700

    merge code

--- ClientApp/app/src/main/java/com/example/clientapp/ui/ProfileFragment.kt
+++ ClientApp/app/src/main/java/com/example/clientapp/ui/ProfileFragment.kt
@@ -7,16 +7,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
+import com.example.clientapp.MainViewModel
 import com.example.clientapp.R
 import com.example.clientapp.databinding.FragmentProfileBinding
+import com.example.clientapp.utils.ImageConverter
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ProfileFragment : Fragment() {
-
+    private val viewModel: MainViewModel by activityViewModels()
+    
     companion object {
         private const val REQUEST_CODE_PICK_IMAGE = 1001
     }
-
+    
     private lateinit var binding: FragmentProfileBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,30 +32,29 @@ class ProfileFragment : Fragment() {
         binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
     }
-
+    
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.ivAvatar.setOnClickListener {
-            val intent = Intent(Intent.ACTION_PICK)
-            intent.type = "image/*"
-            startActivityForResult(intent, REQUEST_CODE_PICK_IMAGE)
-        }
-
-
-        binding.btnEditProfile.setOnClickListener {
-            val bottomSheet = EditProfileBottomSheetFragment()
-            bottomSheet.show(parentFragmentManager, "EditProfileBottomSheet")
-        }
-
+        val currentUser = viewModel.getCurrentUser()
+        binding.tvName.text = currentUser.name
+        binding.tvUsername.text = currentUser.username
         Glide.with(this)
-            .load(R.drawable.jack)
+            .load(
+                ImageConverter().stringToBitmap(
+                    currentUser.image
+                )
+            )
             .placeholder(R.drawable.avatar_placeholder)
             .error(R.drawable.avatar_error)
             .circleCrop()
             .into(binding.ivAvatar)
+        
+        binding.btnProfileLogout.setOnClickListener {
+            viewModel.logout()
+            findNavController().navigate(R.id.action_mainFragment_to_loginFragment)
+        }
     }
-
+    
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_CODE_PICK_IMAGE && resultCode == AppCompatActivity.RESULT_OK) {
@@ -63,6 +69,5 @@ class ProfileFragment : Fragment() {
             }
         }
     }
-
-
+    
 }
